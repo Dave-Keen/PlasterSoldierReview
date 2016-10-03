ed47d6fe81de9e9ebee18f55aa344f1e536dd8df
ibatterbee82 <ian.batterbee@me.com>
2016-10-03 20:33:20 +0100

    New homepage template and updated js
    Home 1.2.html contains updates to the hero section and article listers.
    
    1) The hero section is now less complicated and is completely separate
    to the article listers
    
    2) Each article item on the homepage has the following markup:
    
              <div class="col s12 center-align cta">
                  <a class="waves-effect btn">Read Review</a>
              </div>
    
    The link can be inserted via the above markup. If there is no link
    available then simply keep it blank e.g.,
    
              <div class="col s12 center-align cta">
    
              </div>
    
    3) All JS has been removed from the actual page and added to the
    init.js file. I expect only the carousel JS to be embedded on the
    article page, but all other JS should be moved into this file - I will
    go through all the templates and keep the file up to date, so you will
    probably receive a few more commits.

--- psr/js/init.js
+++ psr/js/init.js
@@ -1,7 +1,40 @@
 (function($){
   $(function(){
 
+  	// side nav
     $('.button-collapse').sideNav();
+
+	// modal
+	$('.modal-trigger').leanModal();
+
+	// select box
+	$('select').material_select();
+
+	// advanced search toggle
+	$('#advancedSearch').addClass('hide'); 
+	$('.advanced-search-toggle').on('click', function() {
+        $('#advancedSearch').toggleClass('hide');
+	});
+
+	// advanced search button toggle
+	$(".read-more-btn-content").click(function () {
+	$(this).text(function(i, text){
+          return text === "Advanced Search" ? "Hide Advanced Search" : "Advanced Search";
+        })
+	});
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+
+
+
+
+
+    
+    
+
+
