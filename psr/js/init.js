cab57434c6f158bd70da9f5854066548fd4d807e
ibatterbee82 <ian.batterbee@me.com>
2016-10-31 22:14:54 +0000

    Sample About Page and updated CSS files

--- psr/js/init.js
+++ psr/js/init.js
@@ -63,11 +63,23 @@
 		})
 	});
 
-	// scroll to top
-		$('#top').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-
   });
+
+// scroll to top
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('#top').fadeIn();
+    } else {
+        $('#top').fadeOut();
+    }
+});
+
+$('#top').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+
 })(jQuery);
